838cb63ef02239fc30fc85cbd4d19e5a5b6c8a89
mcarmona <mcarmona@e3502bb7-87e8-43ed-999b-e522bd66affe>
2016-02-29 02:34:48 +0000

    Agregados Freemarker y Log4j

--- DiccionarioLesco/src/main/java/com/lesco/diccionario/controller/Principal.java
+++ DiccionarioLesco/src/main/java/com/lesco/diccionario/controller/Principal.java
@@ -1,5 +1,6 @@
 package com.lesco.diccionario.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +11,21 @@ import org.springframework.web.servlet.ModelAndView;
 public class Principal {
 
 	String message = "Welcome to Spring MVC!";
+	
+	private static final Logger logger = Logger.getLogger(Principal.class);
 	 
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 		System.out.println("in controller");
+		
+		//logs debug message
+		if(logger.isDebugEnabled()){
+			logger.debug("getWelcome is executed!");
+		}
+		
+		//logs exception
+		logger.error("This is Error message", new Exception("Testing"));
  
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
---
+++ DiccionarioLesco/src/main/webapp/WEB-INF/ftl/helloworld.ftl
@@ -0,0 +1,25 @@
+<html>
+<head><title>ViralPatel.net - FreeMarker Spring MVC Hello World</title>
+<body>
+<div id="header">
+<H2>
+    <a href="http://viralpatel.net"><img height="37" width="236" border="0px" src="http://viralpatel.net/blogs/wp-content/themes/vp/images/logo.png" align="left"/></a>
+    FreeMarker Spring MVC Hello World
+</H2>
+</div>
+ 
+<div id="content">
+
+  <br/>
+  <table class="datatable">
+    <tr>
+        <th>Firstname</th>  <th>Lastname</th>
+    </tr>
+    <tr>
+        <td>${message}</td> <td>${name}</td>
+    </tr>
+  </table>
+ 
+</div>  
+</body>
+</html>  
