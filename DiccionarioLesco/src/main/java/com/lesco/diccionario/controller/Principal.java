83fa8e44a6454652488350b6b5c61977e928a0b4
mcarmona <mcarmona@e3502bb7-87e8-43ed-999b-e522bd66affe>
2016-02-29 03:39:03 +0000

    Agregado Hibernate

--- DiccionarioLesco/src/main/java/com/lesco/diccionario/controller/Principal.java
+++ DiccionarioLesco/src/main/java/com/lesco/diccionario/controller/Principal.java
@@ -1,10 +1,16 @@
 package com.lesco.diccionario.controller;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.lesco.diccionario.dao.CityDAO;
+import com.lesco.diccionario.modelo.City;
 
 
 @Controller
@@ -13,6 +19,33 @@ public class Principal {
 	String message = "Welcome to Spring MVC!";
 	
 	private static final Logger logger = Logger.getLogger(Principal.class);
+	
+	
+	@Autowired
+	private CityDAO cityDAO;
+	
+	@RequestMapping("/principal")
+	public ModelAndView principal(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+		
+		//logs debug message
+		if(logger.isDebugEnabled()){
+			logger.debug("getWelcome is executed!");
+		}
+		
+		List<City> listCities = cityDAO.list();
+		
+		System.out.println("List of cities " + listCities);
+		
+		//logs exception
+		logger.error("This is Error message", new Exception("Testing"));
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
 	 
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
