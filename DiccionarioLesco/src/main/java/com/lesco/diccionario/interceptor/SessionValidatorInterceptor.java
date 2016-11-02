5c68d3b3fe20224aa6fd3c1ad3770c9a56137942
mcarmona <mcarmona@e3502bb7-87e8-43ed-999b-e522bd66affe>
2016-11-02 17:18:22 +0000

    Addded javax support to be able to manipulate Http objects, for the request and the response.
    Added basic interceptor configuration. Added basic session and login logic.

---
+++ DiccionarioLesco/src/main/java/com/lesco/diccionario/interceptor/SessionValidatorInterceptor.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package com.lesco.diccionario.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author m.carmona.dinarte
+ *
+ */
+public class SessionValidatorInterceptor extends HandlerInterceptorAdapter {
+
+	//Log4J class logger instance
+	private static final Logger logger = Logger.getLogger(SessionValidatorInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		long startTime = System.currentTimeMillis();
+		logger.info("Request URL::" + request.getRequestURL().toString()
+				+ ":: Start Time=" + System.currentTimeMillis());
+		request.setAttribute("startTime", startTime);
+		//if returned false, we need to make sure 'response' is sent
+		
+		//TODO Work this logic
+//		HttpSession session = request.getSession(false);
+//		if (session == null) {
+//		    // Not created yet. Now do so yourself.
+//		    session = request.getSession();
+//		} else {
+//		    // Already created.
+//		}
+		
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("Request URL::" + request.getRequestURL().toString()
+				+ " Sent to Handler :: Current Time=" + System.currentTimeMillis());
+		//we can add attributes in the modelAndView and use that in the view page
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		long startTime = (Long) request.getAttribute("startTime");
+		logger.info("Request URL::" + request.getRequestURL().toString()
+				+ ":: End Time=" + System.currentTimeMillis());
+		logger.info("Request URL::" + request.getRequestURL().toString()
+				+ ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+	}
+	
+}
---
+++ DiccionarioLesco/src/main/java/com/lesco/diccionario/pojo/LoginForm.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.lesco.diccionario.pojo;
+
+/**
+ * LoginForm POJO
+ * 
+ * @author Mario Alonso Carmona Dinarte
+ * @email monacar89@hotmail.com
+ * @since 2016
+ *
+ */
+
+public class LoginForm{
+
+	private String emailAddress;
+	private String password;
+	
+	/**
+	 * @return the emailAddress
+	 */
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	/**
+	 * @param emailAddress the emailAddress to set
+	 */
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
